5306f92dedbbe3339cac54204d795d7734db9a24
Martin <mnikolov00@gmail.com>
2018-11-02 11:56:03 +0200

    Add more functionality

--- TodoApp.js
+++ TodoApp.js
@@ -1,78 +1,113 @@
-const usernameField = document.getElementById('usernameField');
-const head = document.getElementsByClassName('head');
-const textField = document.getElementById('inputText');
-const logBtn = document.getElementById('btnLogIn');
+((win, doc) => {
+	const usernameField = doc.getElementById('usernameField');
+	const head = doc.getElementsByClassName('head');
+	const textField = doc.getElementById('inputText');
+	const logBtn = doc.getElementById('btnLogIn');
+	const createBtn = doc.getElementById('btnCreate');
+	const noTodos = doc.getElementById('noTodos');
 
-let greet = '';
-let input = '';
-let todo = '';
-let username = '';
+	let greet = '';
+	let input = '';
+	let todo = '';
+	let username = '';
 
-// Log User
-function logUser() {
-	console.log(`'1- ${username}`);
+	// Log User
+	logBtn.addEventListener('click', logUser);
 
-	if (logBtn.innerHTML == 'Log in') {
-		console.log(usernameField.value);
-		username = usernameField.value;
+	function logUser() {
+		console.log(`'1- ${username}`);
 
-		console.log(`'2-' ${username}`);
-		if (!username) {
-			alert('You must write Username!');
-			clear();
-			username = '';
+		if (logBtn.innerHTML == 'Log in') {
+			console.log(usernameField.value);
+			username = usernameField.value;
+
+			console.log(`'2-' ${username}`);
+			if (!username) {
+				alert('You must write Username!');
+				clear();
+				username = '';
+			} else {
+				greet = doc.createElement('p');
+				greet.innerHTML = `Hello <strong>${username}</strong>`;
+				head[0].prepend(greet);
+				usernameField.setAttribute('style', 'display:none');
+				logBtn.innerHTML = 'Log out';
+			}
 		} else {
-			greet = document.createElement('p');
-			greet.innerHTML = `Hello <strong>${username}</strong>`;
-			head[0].prepend(greet);
-			usernameField.setAttribute('style', 'display:none');
-			logBtn.innerHTML = 'Log out';
+			greet.setAttribute('style', 'display:none');
+			usernameField.setAttribute('style', 'display:block');
+			logBtn.innerHTML = 'Log in';
+			console.log(`'3-' ${username}`);
+			clear();
 		}
-	} else {
-		greet.setAttribute('style', 'display:none');
-		usernameField.setAttribute('style', 'display:block');
-		logBtn.innerHTML = 'Log in';
-		console.log(`'3-' ${username}`);
-		clear();
-	}
-}
 
-// create new todo
-
-function newTodo() {
-	let del = document.createElement('span');
-	del.innerHTML = 'X';
-	del.classList.add('right');
-	del.setAttribute('onclick', 'deleteTodo()');
-	del.setAttribute('id', 'delTodo');
-
-	let li = document.createElement('li');
-	li.classList.add('fLex');
-	let input = document.getElementById('inputText');
-	let inputText = input.value;
-
-	let todo = document.createTextNode(`${username} ${inputText}`);
-	li.appendChild(todo);
-	li.appendChild(del);
-	if (inputText == '') {
-		alert('You must write something!');
-	} else {
-		document.getElementById('ul').appendChild(li);
+		usernameField.value = '';
 	}
 
-	input.value = '';
-}
+	// create new todo
+	createBtn.addEventListener('click', newTodo);
 
-// Delete
-function deleteTodo() {
-	let that = document.getElementById('delTodo');
-	let li = that.parentElement;
-	let ul = that.parentElement.parentElement;
-	console.log(ul);
-	ul.removeChild(li);
-}
-//clear
+	function newTodo() {
+		const deleteBtn = doc.createElement('span');
+		const user = doc.createElement('span');
+		user.innerHTML = `${username}:`;
+		user.classList.add('userStyle');
 
-function clear() {
-	usernameField.value = '';
-}
+		//Add X button
+		deleteBtn.innerHTML = 'X';
+		deleteBtn.classList.add('close');
+		deleteBtn.addEventListener('click', deleteTodo);
+		deleteBtn.setAttribute('id', 'delTodo');
+
+		// create li el.
+		const li = doc.createElement('li');
+		li.classList.add('fLex');
+		li.classList.add('style');
+		li.addEventListener('click', done);
+
+		let inputText = textField.value;
+
+		//create todo
+		let todo = doc.createTextNode(`${inputText}`);
+		li.appendChild(todo);
+		li.appendChild(deleteBtn);
+		li.appendChild(user);
+		if (inputText == '') {
+			alert('You must write something!');
+		} else {
+			if (username == '') {
+				alert('First Log in Username!');
+			} else {
+				doc.getElementById('ul').appendChild(li);
+				noTodos.setAttribute('style', 'display:none');
+				clear();
+			}
+		}
+		//clear input value
+		textField.value = '';
+
+		//done
+		function done() {
+			li.classList.toggle('checked');
+		}
+
+		//check li
+	}
+
+	// Delete
+	function deleteTodo() {
+		let that = doc.getElementById('delTodo');
+		let li = that.parentElement;
+		let ul = that.parentElement.parentElement;
+		alert('Are you sure you want to delete this item?');
+		ul.removeChild(li);
+
+		if (ul.childNodes.length == 1) {
+			noTodos.setAttribute('style', 'display:block');
+		}
+	}
+	//clear
+	function clear() {
+		usernameField.value = '';
+	}
+})(window, document);
