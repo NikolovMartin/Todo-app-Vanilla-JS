4c6379e7f2137ca13834b814b7e8497b741f0aef
Martin <mnikolov00@gmail.com>
2018-11-06 15:54:45 +0200

    Make it class

--- TodoApp.js
+++ TodoApp.js
@@ -1,109 +1,121 @@
-function todoApp(win, doc) {
-	this.usernameField = doc.getElementById('usernameField');
-	this.head = doc.getElementsByClassName('head');
-	this.textField = doc.getElementById('inputText');
-	this.logBtn = doc.getElementById('btnLogIn');
-	this.createBtn = doc.getElementById('btnCreate');
-	this.noTodos = doc.getElementById('noTodos');
+class TodoApp {
+	constructor(win, doc) {
+		this.win = win;
+		this.doc = doc;
 
-	this.greet = doc.createElement('p');
-	this.username = '';
+		// Varaibles
+		this.usernameField = doc.getElementById('usernameField');
+		this.head = doc.getElementsByClassName('head');
+		this.textField = doc.getElementById('inputText');
+		this.logBtn = doc.getElementById('btnLogIn');
+		this.createBtn = doc.getElementById('btnCreate');
+		this.noTodos = doc.getElementById('noTodos');
+		this.greet = doc.createElement('p');
+		this.username = '';
 
-	// Log User
+		// Methods
+		this.logUser = this.logUser.bind(this);
+		this.newTodo = this.newTodo.bind(this);
+		this.deleteTodo = this.deleteTodo.bind(this);
+		this.clear = this.clear.bind(this);
 
-	this.logUser = () => {
-		username = this.usernameField.value;
+		// Event Listeners
+		this.logBtn.addEventListener('click', this.logUser);
+		this.createBtn.addEventListener('click', this.newTodo);
+	}
+
+	logUser() {
+		this.username = this.usernameField.value;
 
 		if (this.logBtn.innerHTML == 'Log out') {
 			this.greet.setAttribute('style', 'display:none');
-			this.toggleVisibility(usernameField, true);
+			this.toggleVisibility(this.usernameField, true);
 			this.setLogBtnText('Log in');
 			this.clear();
 			return;
 		}
 
-		if (!username) {
+		if (!this.username) {
 			alert('You must write Username!');
 			this.clear();
 			return;
 		}
 
-		this.greet.innerHTML = `Hello <strong>${username}</strong>`;
+		this.greet.innerHTML = `Hello <strong>${this.username}</strong>`;
 		this.greet.setAttribute('style', 'display:block');
 		this.head[0].prepend(this.greet);
 		this.toggleVisibility(this.usernameField, false);
 		this.setLogBtnText('Log out');
 		//this.clear();
-	};
+	}
 
 	// create new todo
 
-	this.newTodo = () => {
+	newTodo() {
 		if (!this.textField.value) {
 			alert('You must write something!');
 			return;
 		}
 
-		if (!username) {
-			console.log(username);
+		if (!this.username) {
 			alert('First Log in Username!');
 			return;
 		}
 
-		const deleteBtn = doc.createElement('span');
-		const user = doc.createElement('span');
+		const deleteBtn = this.doc.createElement('span');
+		const user = this.doc.createElement('span');
 
-		user.innerHTML = `${username}:`;
+		user.innerHTML = `${this.username}:`;
 		user.classList.add('userStyle');
 
 		this.addXBtn(deleteBtn);
 		this.createListItem(user, deleteBtn);
-	};
+	}
 
-	this.addXBtn = deleteBtn => {
+	addXBtn(deleteBtn) {
 		deleteBtn.innerHTML = 'X';
 		deleteBtn.classList.add('close');
 		deleteBtn.addEventListener('click', this.deleteTodo);
 		deleteBtn.setAttribute('id', 'delTodo');
-	};
+	}
 
-	this.createListItem = (user, deleteBtn) => {
-		const li = doc.createElement('li');
+	createListItem(user, deleteBtn) {
+		const li = this.doc.createElement('li');
 		li.classList.add('fLex');
 		li.classList.add('style');
 		li.addEventListener('click', () => {
 			this.checked(li);
 		});
 
-		inputText = this.textField.value;
+		this.inputText = this.textField.value;
 
 		//append to li
-		let todo = doc.createTextNode(`${inputText}`);
+		let todo = this.doc.createTextNode(`${this.inputText}`);
 		li.appendChild(todo);
 		li.appendChild(deleteBtn);
 		li.appendChild(user);
 
-		doc.getElementById('ul').appendChild(li);
-		this.toggleVisibility(noTodos, false);
+		this.doc.getElementById('ul').appendChild(li);
+		this.toggleVisibility(this.noTodos, false);
 		this.textField.value = '';
-	};
+	}
 
-	this.checked = li => {
+	checked(li) {
 		li.classList.toggle('checked');
-	};
+	}
 
-	this.toggleVisibility = (item, show) => {
+	toggleVisibility(item, show) {
 		const display = show ? 'block' : 'none';
 		item.setAttribute('style', `display: ${display}`);
-	};
+	}
 
-	this.setLogBtnText = text => {
+	setLogBtnText(text) {
 		this.logBtn.innerHTML = text;
-	};
+	}
 
 	// Delete
-	this.deleteTodo = () => {
-		let that = doc.getElementById('delTodo');
+	deleteTodo() {
+		let that = this.doc.getElementById('delTodo');
 		let li = that.parentElement;
 		let ul = that.parentElement.parentElement;
 		alert('Are you sure you want to delete this item?');
@@ -112,15 +124,14 @@ function todoApp(win, doc) {
 		if (ul.childNodes.length === 1) {
 			this.toggleVisibility(noTodos, true);
 		}
-	};
-	//clear
-	this.clear = () => {
+	}
+	//clear;
+	clear() {
 		usernameField.value = '';
-	};
+	}
 
-	this.logBtn.addEventListener('click', this.logUser);
-	this.createBtn.addEventListener('click', this.newTodo);
-	return this;
+	//	this.logBtn.addEventListener('click', logUser);
+	//  createBtn.addEventListener('click', newTodo);
+	//return this;
 }
-
-const instance = new todoApp(window, document);
+const App2 = new TodoApp(window, document);
